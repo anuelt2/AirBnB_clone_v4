d44f2cbe2da9bcbd1e8a6c9a3be8adb94467ec5f
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-22 00:06:50 +0000

    Add static/scripts/2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+/* global $ */
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  const selectedAmenities = {};
+
+  $('input[type=checkbox]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
+  });
+});
